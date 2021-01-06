e9c1881dde5a9406d1b44ed6da16f42a8e5d4f6a
fr-kan5 <manwith1713@gmail.com>
2021-01-06 16:40:43 +0900

    モデルの単体テスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :password_confirmation
+  end
+
+  with_options presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
 
 end
