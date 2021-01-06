4414af981b3333bfdc7b3211049da8267e78efc0
fr-kan5 <manwith1713@gmail.com>
2021-01-07 03:16:02 +0900

    ActiveHashを利用するモデルの作成と商品モデルとのアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_fee
+  belongs_to :shipping_area
+  belongs_to :shipping_day
+  belongs_to :user
   has_one_attached :image
 end
