f16e8b36cb8f3683739e900369271b8f86789a91
fr-kan5 <manwith1713@gmail.com>
2021-01-07 19:44:19 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,25 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :delivery_fee_id
+    validates :shipping_area_id
+    validates :shipping_day_id
+  end
+
+  validates :price, numericality: { greater_than_or_equal_to: 300 }
+  validates :price, numericality: { less_than_or_equal_to: 9_999_999 }
+
+  validates :price, format: { with: /\A-?[0-9]+(\.[0-9]+)?\z/ }
+
 end
