e6679713fbbbd7fb5aab32c04cad3b73c9d6c127
fr-kan5 <manwith1713@gmail.com>
2021-01-14 07:58:51 +0900

    JavaScriptでtokenを生成してサーバーサイドへ送信する

---
+++ app/javascript/card.js
@@ -0,0 +1,28 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_aa56ca169ffe43b3305707e3");    
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("item_order[number]"),
+      cvc: formData.get("item_order[cvc]"),
+      exp_month: formData.get("item_order[exp_month]"),
+      exp_year: `20${formData.get("item_order[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
