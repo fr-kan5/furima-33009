e5e6cc045621e1619feafb05a9f3c75f9fb688d0
fr-kan5 <manwith1713@gmail.com>
2021-01-16 20:26:42 +0900

    sold outの設定と売却済み商品の商品購入ページへ遷移設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -32,7 +32,7 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id == @item.user.id
+    if current_user.id == @item.user.id || @item.order
       redirect_to root_path
     end
   end
